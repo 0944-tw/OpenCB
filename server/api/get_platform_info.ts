c2161a0ff20d49feb302bb8baaa403521520c15d
zeroninefourfour <zwec181818@proton.me>
2025-03-10 21:45:46 +0800

    feat: Get platform info

---
+++ server/api/get_platform_info.ts
@@ -0,0 +1,10 @@
+export default defineEventHandler(async (event) => {
+     // get platform info
+    var data = {
+     platform: "unknown",
+     region: "unknown",
+    }
+    if (process.env.VERCEL) data.platform = "vercel";
+    if (process.env.NETLIFY) data.platform = "netlify";
+    
+  });
