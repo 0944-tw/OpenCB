9b5b89135069bbaa74ef20910d8fefcee2319057
zeroninefourfour <zwec181818@proton.me>
2025-03-12 21:32:14 +0800

    fix: Default Font For OpenCB

--- app/app.vue
+++ app/app.vue
@@ -4,4 +4,16 @@
       <NuxtPage />
     </v-app>
   </NuxtLayout>
+
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+
+body {
+  font-family: 'Noto Sans TC', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
+ 
