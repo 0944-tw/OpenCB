276875bcdd615486cc1c706c52fe5c80ad315f47
zeroninefourfour <zwec181818@proton.me>
2025-03-29 13:09:38 +0800

    feat: Pending Page Should Shows Pending Posts

--- app/pages/admin/pending.vue
+++ app/pages/admin/pending.vue
@@ -1,18 +1,7 @@
 <template>
-  <v-dialog
-      v-model="loading_modal"
-      max-width="320"
-
-  >
-    <v-list
-        class="py-2"
-        color="green"
-        elevation="12"
-        rounded="lg"
-    >
-      <v-list-item
-          title="已提出審核通過請求..."
-      >
+  <v-dialog v-model="loading_modal" max-width="320">
+    <v-list class="py-2" color="green" elevation="12" rounded="lg">
+      <v-list-item title="已提出審核通過請求...">
         <template v-slot:prepend>
           <div class="pe-4">
             <v-icon color="primary" size="x-large"></v-icon>
@@ -20,59 +9,34 @@
         </template>
 
         <template v-slot:append>
-          <v-progress-circular
-              color="primary"
-              indeterminate="disable-shrink"
-              size="16"
-              width="2"
-          ></v-progress-circular>
+          <v-progress-circular color="primary" indeterminate="disable-shrink" size="16" width="2"></v-progress-circular>
         </template>
       </v-list-item>
     </v-list>
   </v-dialog>
-  <v-dialog
-      v-model="approve_modal"
-      width="auto"
-  >
-    <v-card
-        max-width="400"
-        prepend-icon="mdi-help"
-        text="是否確定通過"
-        title="請確認是否通過此文章"
-    >
+  <v-dialog v-model="approve_modal" width="auto">
+    <v-card max-width="400" prepend-icon="mdi-help" text="是否確定通過" title="請確認是否通過此文章">
       <template v-slot:actions>
-        <v-btn
-            class="ms-auto"
-            text="通過"
-            @click="approve_post(1)"
-        ></v-btn>
+        <v-btn class="ms-auto" text="通過" @click="approve_post(1)"></v-btn>
       </template>
     </v-card>
   </v-dialog>
   <v-dialog v-model="error_modal">
-    <v-card
-        class="mx-auto"
-        prepend-icon="mdi-alert-circle"
-        width="400"
-        color="red"
-    >
+    <v-card class="mx-auto" prepend-icon="mdi-alert-circle" width="400" color="red">
       <template v-slot:title>
         <span class="font-weight-black">請求失敗</span>
       </template>
 
       <v-card-text class="bg-surface-light pt-4">
         <v-row align="center" no-gutters>
-          <v-col
-              class="text-h2"
-              cols="6"
-          >
+          <v-col class="text-h2" cols="6">
             403
           </v-col>
 
 
         </v-row>
       </v-card-text>
- 
+
     </v-card>
   </v-dialog>
   <v-container class="py-8 px-6">
@@ -84,15 +48,8 @@
 
             <v-data-table :items="review_posts" :headers="headers" item-key="title">
               <template v-slot:item.actions="{ item }">
-                <v-btn
-                    icon="mdi-check"
-                    variant="text"
-                    @click="approve_modal = true"
-                />
-                <v-btn
-                    icon="mdi-delete"
-                    variant="text"
-                />
+                <v-btn icon="mdi-check" variant="text" @click="approve_modal = true" />
+                <v-btn icon="mdi-delete" variant="text" />
 
               </template>
             </v-data-table>
@@ -104,35 +61,22 @@
 </template>
 
 <script type="ts" setup>
-import {ref} from 'vue';
+import { ref } from 'vue';
 
 const approve_modal = ref(false)
 const loading_modal = ref(false)
 const error_modal = ref(false)
-const approve_post = (id) => {
-  approve_modal.value = false
-  setTimeout(() => loading_modal.value = true, 250)
-  setTimeout(() => {
-    loading_modal.value = false
-    setTimeout(() => error_modal.value = true, 250)
-  }, 2000)
-}
-const headers = [
-  {title: '標題', value: 'title'},
 
-  {title: "內容", value: "content"},
-  {title: 'IP位置', value: 'ip'},
-  {title: '文章ID', value: 'id'},
-  {title: "創建日期", value: "date"},
-  {title: "動作", value: "actions"}
-]
-const review_posts = [
-  {
-    title: "八年七班小特是傻逼，請各位避開",
-    content: "八年七班小特是傻逼，請各位避開",
-    id: 0,
-    ip: "127.0.0.1",
-    date: "2024-03-12 00:00:00",
-    actions: ""
-  },
-];</script>
+const headers = [
+  { title: '文章ID', value: 'id' },
+  { title: "內容", value: "content" },
+  { title: 'IP位置', value: 'ip' },
+  { title: "創建日期", value: "date" },
+  { title: "動作", value: "actions" }
+];
+console.log("Data Fetched")
+const { data } = useFetch("/api/admin/articles/list")
+const json = JSON.parse(data)
+console.log(json)
+
+</script>
