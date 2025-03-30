257f15700462d850ae0f4c2a074ac1f26bf64584
zeroninefourfour <zwec181818@proton.me>
2025-03-30 14:37:31 +0800

    feat: Better Approved & Pending Page

--- app/pages/admin/approved.vue
+++ app/pages/admin/approved.vue
@@ -1,40 +1,85 @@
-
-
-<script type="ts" setup>
-const review_posts = [
-  {
-    title: "八年七班小特是傻逼，請各位避開",
-    content: "八年七班小特是傻逼，請各位避開",
-    id: 0,
-  },
-];</script>
 <template>
+  <v-dialog v-model="reject_modal" width="auto">
+    <v-card max-width="400">
+      <template v-slot:title>是否將狀態設成拒絕</template>
+      <template v-slot:subtitle>更改文章狀態為拒絕</template>
+      <template v-slot:text>
+        您確定要將這篇文章的狀態更改為拒絕嗎？
+      </template>
+      <template v-slot:actions>
+        <v-btn text="取消" @click="reject_modal = false"></v-btn>
+        <v-btn text="確定" @click="rejectPost(current_uuid)"></v-btn>
+      </template>
+    </v-card>
+  </v-dialog>
+
   <v-container class="py-8 px-6">
     <v-row>
       <v-col cols="12">
         <v-card>
           <v-list lines="two">
-            <v-list-subheader>已審核</v-list-subheader>
+            <v-list-subheader>已經通過</v-list-subheader>
 
-            <template v-for="post in review_posts" :key="post.id">
-              <v-list-item>
-                <template v-slot:prepend>
-                  <v-avatar></v-avatar>
-                </template>
+            <v-data-table :items="articles" :headers="headers" item-key="id" :loading="is_loading">
+              <template v-slot:loading>
+                <v-skeleton-loader type="table-row@5"></v-skeleton-loader>
+              </template>
 
-                <v-list-item-title> {{ post.title }}
-                  <v-chip> #{{ post.id }}</v-chip>
-                </v-list-item-title>
-
-                <v-list-item-subtitle>{{
-                    post.content
-                  }}
-                </v-list-item-subtitle>
-              </v-list-item>
-            </template>
+              <template v-slot:item.actions="{ item }">
+                <v-btn icon="mdi-gavel" variant="text" @click="showRejectModal(item.id)" />
+                <v-btn icon="mdi-delete" variant="text" />
+              </template>
+            </v-data-table>
           </v-list>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { Article } from '@/types';
+import { fa } from 'vuetify/locale';
+const is_loading = ref(false);
+const reject_modal = ref(false);
+const current_uuid = ref("");
+
+const articles = ref<Article[]>([]);
+
+const headers = [
+  { title: '文章ID', value: 'id' },
+  { title: "內容", value: "content" },
+  { title: 'IP位置', value: 'ip' },
+  { title: "創建日期", value: "createdAt" },
+  { title: "動作", value: "actions" }
+];
+
+const loadArticles = async () => {
+  try {
+    is_loading.value = true;
+    const lists: Article[] = await $fetch("/api/admin/articles/list?type=approved");
+    articles.value = lists || [];
+    is_loading.value = false;
+  } catch (error) {
+    console.error("Error loading articles:", error);
+  }
+};
+
+const rejectPost = async (uuid: string) => {
+  try {
+    await $fetch(`/api/admin/articles/${uuid}/reject`, { method: "POST" });
+    reject_modal.value = false;
+    loadArticles();
+  } catch (error) {
+    console.error("Error rejecting post:", error);
+  }
+};
+
+const showRejectModal = (uuid: string) => {
+  reject_modal.value = true;
+  current_uuid.value = uuid;
+};
+
+onMounted(loadArticles);
+</script>
--- app/pages/admin/pending.vue
+++ app/pages/admin/pending.vue
@@ -1,44 +1,19 @@
 <template>
-  <v-dialog v-model="loading_modal" max-width="320">
-    <v-list class="py-2" color="green" elevation="12" rounded="lg">
-      <v-list-item title="已提出審核通過請求...">
-        <template v-slot:prepend>
-          <div class="pe-4">
-            <v-icon color="primary" size="x-large"></v-icon>
-          </div>
-        </template>
 
-        <template v-slot:append>
-          <v-progress-circular color="primary" indeterminate="disable-shrink" size="16" width="2"></v-progress-circular>
-        </template>
-      </v-list-item>
-    </v-list>
-  </v-dialog>
   <v-dialog v-model="approve_modal" width="auto">
-    <v-card max-width="400" prepend-icon="mdi-help" text="是否確定通過" title="請確認是否通過此文章">
+    <v-card max-width="400">
+      <template v-slot:title>通過此文章</template>
+      <template v-slot:subtitle>將文章狀態設成通過</template>
+      <template v-slot:text>
+        您確定要將這篇文章的狀態更改為通過嗎?
+      </template>
       <template v-slot:actions>
-        <v-btn class="ms-auto" text="通過" @click="approve_post(1)"></v-btn>
+        <v-btn text="取消" @click="approve_modal = false"></v-btn>
+        <v-btn text="確定" @click="approvePost(current_uuid)"></v-btn>
       </template>
     </v-card>
   </v-dialog>
-  <v-dialog v-model="error_modal">
-    <v-card class="mx-auto" prepend-icon="mdi-alert-circle" width="400" color="red">
-      <template v-slot:title>
-        <span class="font-weight-black">請求失敗</span>
-      </template>
 
-      <v-card-text class="bg-surface-light pt-4">
-        <v-row align="center" no-gutters>
-          <v-col class="text-h2" cols="6">
-            403
-          </v-col>
-
-
-        </v-row>
-      </v-card-text>
-
-    </v-card>
-  </v-dialog>
   <v-container class="py-8 px-6">
     <v-row>
       <v-col cols="12">
@@ -46,9 +21,12 @@
           <v-list lines="two">
             <v-list-subheader>待審核</v-list-subheader>
 
-            <v-data-table :items="review_posts" :headers="headers" item-key="title">
+            <v-data-table :items="articles" :headers="headers" item-key="title" :loading="is_loading">
+              <template v-slot:loading>
+                <v-skeleton-loader type="table-row@5"></v-skeleton-loader>
+              </template>
               <template v-slot:item.actions="{ item }">
-                <v-btn icon="mdi-check" variant="text" @click="approve_modal = true" />
+                <v-btn icon="mdi-check" variant="text" @click="showApproveModal(item.id)" />
                 <v-btn icon="mdi-delete" variant="text" />
 
               </template>
@@ -60,23 +38,52 @@
   </v-container>
 </template>
 
-<script type="ts" setup>
-import { ref } from 'vue';
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import { Article } from '@/types';
 
 const approve_modal = ref(false)
+const is_loading = ref(false)
 const loading_modal = ref(false)
-const error_modal = ref(false)
+const current_uuid = ref("")
+
+const articles = ref<Article[]>([])
+
 
 const headers = [
-  { title: '文章ID', value: 'id' },
   { title: "內容", value: "content" },
   { title: 'IP位置', value: 'ip' },
-  { title: "創建日期", value: "date" },
+  { title: "創建日期", value: "createdAt" },
+  { title: '文章ID', value: 'id' },
   { title: "動作", value: "actions" }
 ];
-console.log("Data Fetched")
-const { data } = useFetch("/api/admin/articles/list")
-const json = JSON.parse(data)
-console.log(json)
+
+const loadArticles = async () => {
+  try {
+    is_loading.value = true;
+    const lists: Article[] = await $fetch("/api/admin/articles/list?type=pending");
+    articles.value = lists || [];
+    is_loading.value = false;
+  } catch (error) {
+    console.error("Error loading articles:", error);
+  }
+}
+const approvePost = async (uuid: string) => {
+  const approve = await $fetch(`/api/admin/articles/${uuid}/approve`, {
+    method: "POST"
+  })
+  approve_modal.value = false
+  articles.value = []
+  loadArticles()
+
+}
+
+const showApproveModal = async (uuid: string) => {
+  approve_modal.value = true
+  current_uuid.value = uuid
+}
+onMounted(async () => {
+  loadArticles()
+})
 
 </script>
