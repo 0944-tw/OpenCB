ceb6bffe5687d53a69588bc80ab48194f9eb1dc9
zeroninefourfour <zwec181818@proton.me>
2025-04-05 14:46:15 +0800

    fix: Types

--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,12 @@
 export interface Article {
-    id: string;
+    id: number;
     imageUrl: string | null;
     status: string;
     ip: string | null;
     createdAt: Date;
     rejectedReason: string | null;
     content: string;
+    delete_key: string;
 }
 
  
